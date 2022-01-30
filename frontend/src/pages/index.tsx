0edd10bbd07161b6da3db671aa48426d34deeba0
francisboulianne <francis.boulianne1@hotmail.com>
2022-01-30 14:42:01 -0500

    refactor

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { LandingPage } from "../components/LandingPage";
+import { HomePage } from "../components/HomePage";
 
 const Home: NextPage = () => {
   return (
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Rocketeer</title>
       </Head>
-      <LandingPage />
+      <HomePage />
     </>
   );
 };
