7f60ccc644d530354a9ba9fc4225c75e5d9a93a2
francisboulianne <francis.boulianne1@hotmail.com>
2022-02-03 14:07:55 -0500

    add logout option

--- frontend/src/components/profile/ProfilePage.tsx
+++ frontend/src/components/profile/ProfilePage.tsx
@@ -1,10 +1,17 @@
 import Link from "next/link";
+import { useCookies } from "react-cookie";
 import { FormHeader } from "../shared/FormHeader";
 import { FormLayout } from "../shared/FormLayout";
 import { MenuItem } from "../shared/MenuItem";
 import { ProfileInformation } from "./ProfileInformation";
 
 export const ProfilePage = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(["credentials"]);
+
+  const logOut = () => {
+    removeCookie("credentials");
+  };
+
   return (
     <div>
       <MenuItem variant="xl">
@@ -15,8 +22,10 @@ export const ProfilePage = () => {
       </MenuItem>
 
       <MenuItem variant="sm">
-        <Link href="/">
-          <a className="text-sky-500 font-semibold">Log out</a>
+        <Link href="/" passHref={true}>
+          <button className="text-white w-20 rounded py-2 mt-2 bg-blue-400" onClick={logOut}>
+            Log out
+          </button>
         </Link>
       </MenuItem>
     </div>
