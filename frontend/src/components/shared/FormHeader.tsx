805fde574889b6564b6d9937b88c416a80c3a274
francisboulianne <francis.boulianne1@hotmail.com>
2022-02-04 16:31:08 -0500

    add relative path to image

--- frontend/src/components/shared/FormHeader.tsx
+++ frontend/src/components/shared/FormHeader.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const FormHeader = ({ text }: Props) => {
   return (
     <div className="content flex justify-center py-4 pb-6 mb-4 relative">
-      <img className="object-contain h-20 w-30" src="/images/rocket.jpg" alt="Rocket Logo" />
+      <img className="object-contain h-20 w-30" src="./images/rocket.jpg" alt="Rocket Logo" />
       {text && <h1 className="font-serif text-5xl font-semibold mt-4 text-sky-500">{text}</h1>}
     </div>
   );
