591cb56fc8106e40b7b7f272f79ad9c1c7352ac4
francisboulianne <francis.boulianne1@hotmail.com>
2022-03-03 11:45:39 -0500

    change server address

--- frontend/src/config/config.production.ts
+++ frontend/src/config/config.production.ts
@@ -2,6 +2,6 @@ import { Config } from ".";
 
 export const productionConfig: Config = {
   server: {
-    address: "http://onlytanksbackend-env.eba-w3ecpqtz.us-east-1.elasticbeanstalk.com"
+    address: "https://rocketeer-wep-app.herokuapp.com/"
   }
 };
